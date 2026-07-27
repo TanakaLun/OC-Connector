925d99651225b08c1880d3c9dbaa451ec342b11e
TanakaLun <2709410138@qq.com>
2026-07-27 20:38:21 +0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file
 plugins {
-    id("com.android.application" apply false
+    id("com.android.application") apply false
     id("org.jetbrains.kotlin.android") apply false
     id("org.jetbrains.kotlin.plugin.serialization") apply false
     id("com.google.dagger.hilt.android") apply false
